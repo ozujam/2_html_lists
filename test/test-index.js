6912ff5d6d0b2fc058e54f63e981366daaafafc6
ozubergz <jmin499@gmail.com>
2020-10-27 15:47:11 -0400

    check unordered lists

--- test/test-index.js
+++ test/test-index.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const { expect, should } = require('chai');
 const chai = require('chai');
 const { JSDOM } = require('jsdom');
 
@@ -15,5 +15,25 @@ describe('index.html', () => {
         .then(done, done);
     });
 
-    
+    describe('Unordered List', () => {
+        it('ul tag should exists', () => {
+            expect(document.querySelector('ul')).to.exist
+        })
+        it('ul should contain THREE li tags', () => {
+            expect(document.querySelector('ul')).to.have.descendants('li').and.have.length(3)
+        })
+        it('lists should not be empty', () => {
+            
+            // find all unordered lists 
+            const lists = document.querySelectorAll('ul li');
+            const string1 = lists[0].textContent
+            const string2 = lists[1].textContent
+            const string3 = lists[2].textContent
+
+            // expect lists to not be empty string
+            expect(string1).to.not.equal('')
+            expect(string2).to.not.equal('')
+            expect(string3).to.not.equal('')
+        })
+    })
 })
